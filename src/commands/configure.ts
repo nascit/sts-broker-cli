0c962211af50ff8228c0ad7b9acf1f6219e927d1
nascit <timoteo.asn@gmail.com>
2020-03-11 11:33:08 +0000

    validating configure entries

--- src/commands/configure.ts
+++ src/commands/configure.ts
@@ -12,31 +12,76 @@ const prompt = async (): Promise<any> => {
   let getRegion: any = await inquirer.prompt([{
     name: 'region',
     message: 'In what region is the STS Broker hosted?',
-    type: 'input'
+    type: 'input',
+    validate: function(value) {
+      var pass = value.match(
+        /(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\d/g
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid AWS region';
+    }
   }])
 
   let getEndpoint: any = await inquirer.prompt([{
     name: 'endpoint',
     message: 'What is your STS Broker endpoint? (eg.: "xxxxx.execute-api.us-east-2.amazonaws.com/Prod")',
-    type: 'input'
+    type: 'input',
+    validate: function(value) {
+      var pass = value.match(
+        /((www\.)?([\w\-\.\/])*(\.[a-zA-Z]{2,3}\/?))[^\s\b\n|]*[^.,;:\?\!\@\^\$ -]/g
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid endpoint';
+    }
   }])
 
   let getUserPoolId: any = await inquirer.prompt([{
     name: 'userPoolId',
     message: 'What is your Cognito User Pool ID?',
-    type: 'input'
+    type: 'input',
+    validate: function(value) {
+      var pass = value.match(
+        /(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\d_([A-Za-z0-9]{9,9}$)/g
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid User Pool ID';
+    }
   }])
 
   let getUserPoolWebClientId: any = await inquirer.prompt([{
     name: 'userPoolWebClientId',
-    message: 'What is your Cognito User Pool client ID?',
-    type: 'input'
+    message: 'What is your Cognito User Pool App client ID?',
+    type: 'input',
+    validate: function(value) {
+      var pass = value.match(
+        /^([a-z0-9]{26,26}$)/g
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid User Pool App client ID';
+    }
   }])
 
   let getCognitoDomain: any = await inquirer.prompt([{
     name: 'cognitoDomain',
     message: 'What is your Cognito domain URL? (eg.: "stsbroker.auth.us-east-2.amazoncognito.com")',
-    type: 'input'
+    type: 'input',
+    validate: function(value) {
+      var pass = value.match(
+        /((www\.)?([\w\-\.\/])*(\.[a-zA-Z]{2,3}\/?))[^\s\b\n|]*[^.,;:\?\!\@\^\$ -]/g
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid endpoint';
+    }
   }])
 
   const config = {
